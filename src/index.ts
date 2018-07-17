7279780a9456e5519c6407de5086d651f4a6b24b
James Chong <wei3hua2@gmail.com>
2018-07-17 15:02:37 +0800

    update documentation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import glob from 'glob';
 import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 
-/** Glob Module
+/** Match files using patterns.
+ * @see {@link https://www.npmjs.com/package/glob|Glob}
  * @namespace Glob
 */
 @RpsModule("glob")
 export default class RPSGlob {
 
+    /**
+ * @function glob
+ * @memberof Glob
+ * @example
+ * ;match files that starts with pa
+ * glob pa*
+ * @param {string} pattern pattern to be matched
+ * @returns {void} 
+ * @summary Match file given a pattern.
+ * 
+ * @see {@link https://www.npmjs.com/package/glob}
+ * 
+*/
   @rpsAction({verbName:'glob'})
   async glob (ctx:RpsContext,opts:Object, pattern:any) : Promise<string[]>{
     return glob.sync(pattern,opts);
